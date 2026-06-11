6b4913c3133fc4f0df1e6ce9db2291985e467285
minhajulofficialcpa-rgb <minhajulofficial.cpa@gmail.com>
2026-06-11 09:17:47 +0600

    Use theme-aware label color

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 
 const Label = React.forwardRef<React.ElementRef<typeof LabelPrimitive.Root>, React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>>(
   ({ className, ...props }, ref) => (
-    <LabelPrimitive.Root ref={ref} className={cn("text-sm font-medium text-white", className)} {...props} />
+    <LabelPrimitive.Root ref={ref} className={cn("text-sm font-medium text-foreground", className)} {...props} />
   )
 );
 Label.displayName = LabelPrimitive.Root.displayName;
